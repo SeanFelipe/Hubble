6440c4a687acd5162d4e78738d559ded7d0dbfeb
Sean Felipe <sean@addlightness.tech>
2021-04-20 17:18:45 -0600

    remove Status from header title

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        PTES Automation Status
+        PTES Automation
       </header>
       {/*
       <Sandbox />
